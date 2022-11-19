3bad2569721712814f26b07179613fc3625d0a15
wuruimiao <dev.wuruimiao@gmail.com>
2022-11-19 10:23:55 +0800

    feat: chongfan gui

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 import sys
+import time
 from collections import defaultdict
 from datetime import datetime, timedelta
 from typing import List
@@ -11,7 +12,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import QIntValidator
 from utils.data_helper import store, get_data
-from utils.time_helper import get_pass_time
+from utils.time_helper import get_pass_time, get_remain_time, format_time
 
 
 def make_one_line(widgets: List[QWidget], box: QBoxLayout) -> QBoxLayout:
@@ -40,6 +41,7 @@ class TimeRecord(object):
     def set_by_key(self, key: str, d: int):
         print(f"set {key} {d}")
         setattr(self, key, d)
+        self.refresh()
         self.empty = False
 
     @property
@@ -55,7 +57,16 @@ class TimeRecord(object):
 
     @property
     def finish_time_str(self) -> str:
-        return self.finish_time.strftime("%Y-%m-%d, %H:%M:%S")
+        return format_time(self.finish_time, "%Y-%m-%d %H:%M")
+
+    def __str(self):
+        return f"day={self.day} hour={self.hour} minute={self.minute} time={format_time(self.t)}"
+
+    def __str__(self):
+        return self.__str()
+
+    def __repr__(self):
+        return self.__str()
 
 
 class ChongFan(QDialog):
@@ -63,7 +74,7 @@ class ChongFan(QDialog):
         super(ChongFan, self).__init__(parent)
         self._db_name = "chongfandiguo.db"
         self._data = get_data(self._db_name)
-        self._item = ("建造1", "建造2", "学院", "城堡", "募兵1", "募兵2", "治疗")
+        self._item = ("建造1", "建造2", "学院", "城堡", "募兵1", "募兵2", "治疗", "招募")
         if self._data is None:
             self._record = defaultdict(TimeRecord)
         else:
@@ -82,6 +93,13 @@ class ChongFan(QDialog):
         box2.addLayout(right_box)
         self.setLayout(box2)
 
+    def _update_by_key_attr(self, record_key, record_item_attr, context):
+        self._record[record_key].set_by_key(record_item_attr, context)
+        # TODO: 异步刷新
+        print(self._record)
+        store(self._record, self._db_name)
+        return True
+
     def _init_record_part(self, now):
         """
         初始化左侧记录面板
@@ -92,22 +110,9 @@ class ChongFan(QDialog):
 
         """
 
-        def update(_lb, i):
-            """
-            输入改变时，存储数据
-            Args:
-                _lb:
-                i:
-
-            Returns:
-
-            """
-
+        def update(record_key, record_item_attr):
             def _update(context):
-                self._record[_lb].set_by_key(i, context)
-                self._record[_lb].refresh()
-                # TODO: 异步刷新
-                store(self._record, self._db_name)
+                self._update_by_key_attr(record_key, record_item_attr, context)
 
             return _update
 
@@ -121,27 +126,27 @@ class ChongFan(QDialog):
             minute = QSpinBox(self)
             minute.setRange(0, 59)
 
-            exist = self._record[item]
-            if not exist.empty:
-                pass_day, pass_hour, pass_minute = get_pass_time(exist.t, now)
-                day.setValue(exist.day - pass_day)
-                hour.setValue(exist.hour - pass_hour)
-                minute.setValue(exist.minute - pass_minute)
+            if item in self._record and not self._record[item].empty:
+                # 再次判断以免直接初始化记录
+                exist = self._record[item]
+                remain_d, remain_h, remain_m = get_remain_time(exist.t, now, exist.day, exist.hour, exist.minute)
+                day.setValue(remain_d)
+                hour.setValue(remain_h)
+                minute.setValue(remain_m)
 
             # 后关联，否则会触发更新
             day.valueChanged[int].connect(update(item, "day"))
             hour.valueChanged[int].connect(update(item, "hour"))
             minute.valueChanged[int].connect(update(item, "minute"))
 
-            lb = QLabel(f"{item}耗时：", self)
+            lb = QPushButton(f"{item}耗时：", self)
+            lb.clicked.connect(lambda x: day.setValue(0) or hour.setValue(11) or minute.setValue(30))
+
             make_one_line([lb,
                            day, QLabel("天", self),
                            hour, QLabel("时", self),
                            minute, QLabel("分", self),
                            ], box)
-        btn = QPushButton("免费招募", self)
-        lb = QLabel("上次招募时间", self)
-        make_one_line([btn, lb], box)
         box.addStretch(1)
         return box
 
@@ -153,9 +158,9 @@ class ChongFan(QDialog):
             if value.is_null:
                 continue
             if value.finished(now):
-                finished.append(QLabel(f"{key} {value.finish_time_str}", self))
+                finished.append(QLabel(f"{key} {value.finish_time_str} 结束", self))
             else:
-                going.append(QLabel(f"{key} {value.finish_time_str}", self))
+                going.append(QLabel(f"{key} {value.finish_time_str} 结束", self))
         self.display_labels(box, finished, "已完成")
         self.display_labels(box, going, "进行中")
         box.addStretch(1)
--- utils/time_helper.py
+++ utils/time_helper.py
@@ -1,10 +1,14 @@
-from datetime import datetime
+from datetime import datetime,timedelta
 from typing import Tuple
+
+
+def format_time(t: datetime, f: str = "%Y-%m-%d %H:%M:%S") -> str:
+    return t.strftime(f)
 
 
 def get_pass_time(early: datetime, latest: datetime) -> Tuple[int, int, int]:
     """
-
+    获取已经过去了多少天、时、分
     Args:
         early:
         latest:
@@ -14,3 +18,21 @@ def get_pass_time(early: datetime, latest: datetime) -> Tuple[int, int, int]:
     """
     d = latest - early
     return d.days, d.seconds // 3600, (d.seconds // 60) % 60
+
+
+def get_remain_time(early: datetime, latest: datetime, day: int, hour: int, minute:int) -> Tuple[int, int, int]:
+    """
+
+    Args:
+        early: 记录生成的时间点
+        latest: 最新的时间点
+        day: 记录的天数
+        hour: 记录的小时数
+        minute: 记录的分钟数
+
+    Returns: 距离记录的截止时间，还有多少天、时、分
+    """
+    deadline = early + timedelta(days=day, hours=hour, minutes=minute)
+    if deadline <= latest:
+        return 0, 0, 0
+    return get_pass_time(latest, deadline)
