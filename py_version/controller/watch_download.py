431d71879896f882394cf497367d60c6118fd674
wuruimiao <dev.wuruimiao@gmail.com>
2022-12-11 15:05:51 +0800

    refactor: use capture_screen

--- py_version/controller/watch_download.py
+++ py_version/controller/watch_download.py
@@ -5,47 +5,42 @@ import numpy as np
 import cv2 as cv
 from PIL import Image
 from mss import mss
-
+from utils.image import capture_screen
 from controller.ai.identify import identify_color
 from controller.img_page import get_bar, get_ensure_save_window
 from controller.gui_thatwind_tool import cancel_download_for_exist, no_refresh_tool_download, finish_download
 
 logger = logging.getLogger(__name__)
 
+state = ""
+
+
+def __watch_download(img):
+    global state
+    # display_img(img)
+    color = identify_color(get_ensure_save_window(img))
+    # TODO: 下载工具页未加载完成
+    if len(color) > 3:
+        # 需要取消下载
+        state = "init"
+        print("cancel download")
+        cancel_download_for_exist()
+    # display_img(get_ensure_save_window(img))
+
+    color = identify_color(get_bar(img))
+    if "blue" in color:
+        pass
+    elif "green" in color:
+        # 下载完成，点击关闭
+        finish_download()
+        state = "init"
+    elif "white" in color and state != "ing":
+        # 开始下载
+        no_refresh_tool_download()
+        state = "ing"
+        pass
+    time.sleep(2)
+
 
 def watch_download():
-    # display_img_by_name("images/color1.png")
-    mon = {'left': 0, 'top': 0, 'width': 3804, 'height': 2160}
-    count = 0
-    state = ""
-    with mss() as sct:
-        while True:
-            cap = sct.grab(mon)
-            img = Image.frombytes('RGB', (cap.width, cap.height), cap.rgb, )
-            img = np.array(img)
-            img = cv.cvtColor(img, cv.COLOR_RGB2BGR)
-            # display_img(img)
-            color = identify_color(get_ensure_save_window(img))
-            # TODO: 下载工具页未加载完成
-            if len(color) > 3:
-                # 需要取消下载
-                state = "init"
-                print("cancel download")
-
-                cancel_download_for_exist()
-            # display_img(get_ensure_save_window(img))
-
-            color = identify_color(get_bar(img))
-            if "blue" in color:
-                pass
-            elif "green" in color:
-                # 下载完成，点击关闭
-                finish_download()
-                state = "init"
-            elif "white" in color and state != "ing":
-                # 开始下载
-                no_refresh_tool_download()
-                state = "ing"
-                pass
-
-            time.sleep(2)
+    capture_screen(__watch_download)
